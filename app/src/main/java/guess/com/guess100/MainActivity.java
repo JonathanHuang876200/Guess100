bd2d104ad8c8fa321a463da5656bcf2400026b60
jonathan <jonathan876200@gmail.com>
2017-11-14 10:20:59 +0800

    Guess

--- app/src/main/java/guess/com/guess100/MainActivity.java
+++ app/src/main/java/guess/com/guess100/MainActivity.java
@@ -1,7 +1,9 @@
 package guess.com.guess100;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,8 +12,9 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     Random ran = new Random();
-    int r = ran.nextInt(99) + 1;
-
+    int r = ran.nextInt(99) + 1;     /*r是SecretNumber*/
+    int max=100;
+    int min =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +22,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView Secret = (TextView)findViewById(R.id.secret);
         TextView Info = (TextView)findViewById(R.id.info);
-        EditText Number = (EditText)findViewById(R.id.number);
         Button Send = (Button)findViewById(R.id.send);
 
+//        int count = Integer.parseInt(edNumber.getText().toString());
         String s = String.valueOf(r);
         Secret.setText(s);
     }
+    public void compare (View view){
+        EditText Number = (EditText)findViewById(R.id.number);
+        int n = Integer.parseInt(Number.getText().toString());
+        if(n==r){
+            new AlertDialog.Builder(MainActivity.this)
+                    .setMessage("猜對了")
+                    .show();
+        }
+        while (n > r &&n > min &&n < max){
+            max = n;
+            new AlertDialog.Builder(MainActivity.this)
+                    .setMessage(min+"to"+max)
+                    .setPositiveButton("OK",null)
+                    .show();
+            break;
+        }
+        while(n < r && n > min && n < max){
+            min = n;
+            new AlertDialog.Builder(MainActivity.this)
+                    .setMessage(min+"to"+max)
+                    .setPositiveButton("OK",null)
+                    .show();
+            break;
+        }
 
+    }
 
 }
